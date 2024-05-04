43f4b390657d42b6c9a5b9bab2a617dd871bcb5a
shorunke999 <shorunke99@gmail.com>
2024-05-04 13:57:28 +0100

    admin route and usertype modification in db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,13 @@ class HomeController extends Controller
     public function index(){
         return view('home');
     }
+
+    public function redirect(){
+        $usertype = auth()->user()->usertype;
+        if($usertype == 1){
+            return view('adminhome');
+        }else{
+            return view('home');
+        }
+    }
 }
